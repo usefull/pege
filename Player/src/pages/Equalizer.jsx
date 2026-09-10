09a8a00b9375bf47e50a13731a30da1016dbc813
usefull <the.usefull@gmail.com>
2026-09-10 17:27:23 +0300

    Equalizer grains layout

--- Player/src/pages/Equalizer.jsx
+++ Player/src/pages/Equalizer.jsx
@@ -26,6 +26,116 @@ const Equalizer = () => {
                     <ToggleSwitch checked={equalizerOn} onChange={e => setEqualizerOn(e.target.checked)}></ToggleSwitch>
                 </div>
             </div>
+            <div className='grains'>
+                <div className='eq-grain'>
+                    <div className='hz-label'>
+                        <span>62Hz</span>
+                        <div className='min-cutoff'></div>
+                    </div>
+                    <input type="range" min={-12} max={12} step={0.5}/>
+                    <div className='hz-label'>
+                        <span>62Hz</span>
+                        <div className='max-cutoff'></div>
+                    </div>
+                    <div className='zero-cutoff'></div>
+                </div>
+                <div className='eq-grain'>
+                    <div className='hz-label'>
+                        <span>125Hz</span>
+                        <div className='min-cutoff'></div>
+                    </div>
+                    <input type="range" min={-12} max={12} step={0.5}/>
+                    <div className='hz-label'>
+                        <span>125Hz</span>
+                        <div className='max-cutoff'></div>
+                    </div>
+                    <div className='zero-cutoff'></div>
+                </div>
+                <div className='eq-grain'>
+                    <div className='hz-label'>
+                        <span>250Hz</span>
+                        <div className='min-cutoff'></div>
+                    </div>
+                    <input type="range" min={-12} max={12} step={0.5}/>
+                    <div className='hz-label'>
+                        <span>250Hz</span>
+                        <div className='max-cutoff'></div>
+                    </div>
+                    <div className='zero-cutoff'></div>
+                </div>
+                <div className='eq-grain'>
+                    <div className='hz-label'>
+                        <span>500Hz</span>
+                        <div className='min-cutoff'></div>
+                    </div>
+                    <input type="range" min={-12} max={12} step={0.5}/>
+                    <div className='hz-label'>
+                        <span>500Hz</span>
+                        <div className='max-cutoff'></div>
+                    </div>
+                    <div className='zero-cutoff'></div>
+                </div>
+                <div className='eq-grain'>
+                    <div className='hz-label'>
+                        <span>1kHz</span>
+                        <div className='min-cutoff'></div>
+                    </div>
+                    <input type="range" min={-12} max={12} step={0.5}/>
+                    <div className='hz-label'>
+                        <span>1kHz</span>
+                        <div className='max-cutoff'></div>
+                    </div>
+                    <div className='zero-cutoff'></div>
+                </div>
+                <div className='eq-grain'>
+                    <div className='hz-label'>
+                        <span>2kHz</span>
+                        <div className='min-cutoff'></div>
+                    </div>
+                    <input type="range" min={-12} max={12} step={0.5}/>
+                    <div className='hz-label'>
+                        <span>2kHz</span>
+                        <div className='max-cutoff'></div>
+                    </div>
+                    <div className='zero-cutoff'></div>
+                </div>
+                <div className='eq-grain'>
+                    <div className='hz-label'>
+                        <span>4kHz</span>
+                        <div className='min-cutoff'></div>
+                    </div>
+                    <input type="range" min={-12} max={12} step={0.5}/>
+                    <div className='hz-label'>
+                        <span>4kHz</span>
+                        <div className='max-cutoff'></div>
+                    </div>
+                    <div className='zero-cutoff'></div>
+                </div>
+                <div className='eq-grain'>
+                    <div className='hz-label'>
+                        <span>8kHz</span>
+                        <div className='min-cutoff'></div>
+                    </div>
+                    <input type="range" min={-12} max={12} step={0.5}/>
+                    <div className='hz-label'>
+                        <span>8kHz</span>
+                        <div className='max-cutoff'></div>
+                    </div>
+                    <div className='zero-cutoff'></div>
+                </div>
+                <div className='eq-grain'>
+                    <div className='hz-label'>
+                        <span>16kHz</span>
+                        <div className='min-cutoff'></div>
+                    </div>
+                    <input type="range" min={-12} max={12} step={0.5}/>
+                    <div className='hz-label'>
+                        <span>16kHz</span>
+                        <div className='max-cutoff'></div>
+                    </div>
+                    <div className='zero-cutoff'></div>
+                </div>
+            </div>
         </div>
     </FadeIn>);
 
