0985788445c048b89c8174ae8b66c48c74703572
usefull <the.usefull@gmail.com>
2026-09-21 12:10:57 +0300

    increased the contrast

--- Player/src/components/Svg.jsx
+++ Player/src/components/Svg.jsx
@@ -1,6 +1,14 @@
 export const FlacSvg = ({ style }) => { return (
-    <svg className='flac-label' viewBox="0 0 17 17" fill="currentColor" fillRule='evenodd' style={style}>
-        <path d="M4 4V12A1 1 0 006 12V10H9A1 1 0 009 8H6V6H12A1 1 0 0012 4ZM8.5 0A1 1 0 008.5 17 1 1 0 008.5 0" />
+    <svg xmlns="http://www.w3.org/2000/svg" className='flac-label' viewBox="0 0 19 19" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" background="transparent" style={style}>
+        <path d="M9.5 1A1 1 0 009.5 18 1 1 0 009.5 1M7 14V6H13M7 10H10"/>
+    </svg>
+);};
+
+export const InfoSvg = ({ style }) => { return (
+    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style={style}>
+        <path d="M12.4545 7.5C12.4545 8.1778 11.905 8.72727 11.2272 8.72727C10.5494 8.72727 9.99996 8.1778 9.99996 7.5C9.99996 6.82219 10.5494 6.27273 11.2272 6.27273C11.905 6.27273 12.4545 6.82219 12.4545 7.5Z" fill="currentColor"/>
+        <path d="M10 11L12 11L12 16L14 16" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        <path d="M20 12C20 16.4183 16.4183 20 12 20C7.58172 20 4 16.4183 4 12C4 7.58172 7.58172 4 12 4C16.4183 4 20 7.58172 20 12Z" stroke="currentColor" strokeWidth="2"/>
     </svg>
 );};
 
--- Player/src/pages/Home.jsx
+++ Player/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router';
 import { useStreams } from '../features/StreamsSlice';
 import { useCurrentStream, useSetCurrentStream, useIsBuffering, useTogglePlay, useMetadata } from "../features/PlayerSlice";
 
-import { ListSvg, EqualizerSvg, FlacSvg} from '../components/Svg';
+import { ListSvg, EqualizerSvg, FlacSvg, InfoSvg } from '../components/Svg';
 import MainButton from '../components/MainButton';
 import FadeIn from '../components/FadeIn';
 import ShiftButton from '../components/ShiftButton';
@@ -51,19 +51,11 @@ const Home = () => {
                 <MainButton title="Play / Stop" onClick={() => togglePlay()}></MainButton>
                 <ShiftButton title="Next stream" onClick={() => setNextStream(true)}></ShiftButton>
             </div>
-            <div className="header-panel">
+            <div className="header-panel" onClick={() => navigate(`/info`)}>
                 <MarqueeText>{currentStreamInfo ? currentStreamInfo.title : ''}</MarqueeText>
                 <NoWrap>
                     <span>{currentStreamInfo ? currentStreamInfo.country : ''}</span>
-                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" onClick={() => navigate(`/info`)}>
-                        <defs>
-                            <radialGradient id="info-button-grad" cx="50%" cy="50%" r="60%" >
-                                <stop offset="0%" stopColor="var(--color1)" />
-                                <stop offset="100%" stopColor="var(--color2)" />
-                            </radialGradient>
-                        </defs>
-                        <path fill="url(#info-button-grad)" fillRule="evenodd" clipRule="evenodd" d="M12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21ZM12 7C11.4477 7 11 7.44772 11 8C11 8.55228 11.4477 9 12 9H12.01C12.5623 9 13.01 8.55228 13.01 8C13.01 7.44772 12.5623 7 12.01 7H12ZM10.5 11C9.94772 11 9.5 11.4477 9.5 12C9.5 12.5523 9.94772 13 10.5 13H11V16C11 16.5523 11.4477 17 12 17H14C14.5523 17 15 16.5523 15 16C15 15.4477 14.5523 15 14 15H13V12C13 11.4477 12.5523 11 12 11H10.5Z"/>
-                    </svg>
+                    <InfoSvg />
                 </NoWrap>
             </div>
             <div className="footer-panel">
